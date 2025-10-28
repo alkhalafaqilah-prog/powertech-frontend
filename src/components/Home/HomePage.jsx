df76ce21dc4fe4e8b210d5d9fb65b97b89df1537
Aqilah Alkhalaf <alkhalafaqilah@gmail.com>
2025-10-28 11:32:54 +0300

    Submitting 28th Oct UPDT2

--- src/components/Home/HomePage.jsx
+++ src/components/Home/HomePage.jsx
@@ -1,7 +1,20 @@
 import Header from './Header'
 import CardContainer from './CardContainer'
+import api from '../../api'
+import { useEffect } from 'react'
 
 const HomePage = () => {
+
+    useEffect(function(){
+        api.get("products")
+        .then (res => {
+        console.log(res.data)
+        })
+
+    .catch(err =>{
+        console.log(err.message)
+    })
+},[])
 
 return (
     <>
