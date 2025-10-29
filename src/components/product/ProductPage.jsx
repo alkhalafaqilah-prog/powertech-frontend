a3a06a04d260fbd2e17859ba6fdf1d7ac14ec6ed
Aqilah Alkhalaf <alkhalafaqilah@gmail.com>
2025-10-29 15:36:21 +0300

    Created funcation useEffect in ProductPage.jsx

--- src/components/product/ProductPage.jsx
+++ src/components/product/ProductPage.jsx
@@ -12,7 +12,23 @@ const ProductPage = () => {
     const [product, setProduct] = useState({})
     const [similarProducts, setSimilarProducts] = useState([])
     const [loading, setLoading] = useState(false)
+    const [inCart, setInCart ] = useState(false)
     const cart_code = localStorage.getItem("cart_code")
+
+    useEffect(function(){
+        if(product.id){
+        api.get(`product_in_cart?cart_code=${cart_code}&product_id=${product.id}`)
+        .then(res => {
+            console.log(res.data)
+            setInCart(res.data.product_in_cart)
+        })
+
+        .catch(err => {
+            console.log(err.message)
+        })
+        }
+
+    },[cart_code, product.id])
 
     const newItem = {cart_code: cart_code, product_id: product.id}
 
@@ -20,6 +36,7 @@ const ProductPage = () => {
         api.post("add_item/", newItem)
         .then(res =>{
             console.log(res.data)
+            setInCart(true)
         })
 
         .catch(err =>{
@@ -74,9 +91,10 @@ const ProductPage = () => {
                 className="btn btn-outline-dark flex-shrink-0"
                 type="button"
                 onClick={add_item}
+                disabled={inCart}
                 >
                 <i className="bi-cart-fill me-1"></i>
-                Add to cart
+                {inCart ? "Product added to cart" : "Add to cart"}
                 </button>
             </div>
             </div>
