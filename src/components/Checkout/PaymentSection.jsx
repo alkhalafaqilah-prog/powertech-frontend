4c0e3e261c19603f15d67dab2889d8e478b56e74
Aqilah Alkhalaf <alkhalafaqilah@gmail.com>
2025-11-03 13:32:17 +0300

    Created makePayment function in PaymentSection.jsx

--- src/components/Checkout/PaymentSection.jsx
+++ src/components/Checkout/PaymentSection.jsx
@@ -1,8 +1,26 @@
 import styles from "./PaymentSection.module.css"
 import { BsFillCreditCard2FrontFill } from "react-icons/bs"
 import { FaCcPaypal } from "react-icons/fa"
+import api from "../../api"
+import { useState } from "react"
 
 const PaymentSection = () => {
+
+    const cart_code = localStorage.getItem("cart_code")
+    const [loading, setLoading] = useState(false)
+
+    function makePayment() {
+        api.post("initiate_payment/", {cart_code})
+        .then(res => {
+            console.log(res.data)
+            //This is to get the link for the payment success
+            window.location.href = res.data.data.link
+        })
+
+        .catch(err => {
+            console.log(err.message)
+        })
+    }
 
     return (
     <div className="col-md-4">
@@ -17,7 +35,7 @@ const PaymentSection = () => {
         </button>
 
         {/* Flutterwave Button */}
-        <button className={`btn btn-warning w-100 ${styles.flutterwaveButton}`} id="flutterwave-button">
+        <button className={`btn btn-warning w-100 ${styles.flutterwaveButton}`} onClick={makePayment} id="flutterwave-button">
         <BsFillCreditCard2FrontFill  style={{fontSize:"30px"}} /> Pay with Flutterwave
         </button>
     </div>
